1e0e2710badefcb3e4f207c259796908a3ba2dad
Holquaria <jholker4@gmail.com>
2023-01-25 15:48:05 +0000

    write function to query db

--- db-seeding/app.js
+++ db-seeding/app.js
@@ -1,23 +1,44 @@
-const mongoose = require('mongoose')
+const { MongoClient } = require("mongodb");
 require('dotenv').config({
     path: `${__dirname}/.env`,
   });
 
-const connection = mongoose.createConnection(process.env.MONGOURI);
-const query = { type: 'annular' }
-const Model = connection.model('EclipsesCluster.annular', new mongoose.Schema({ date: String, type: String, }));
-// Works
-const queryResult = Model.find(query, function(error, result) { 
-    if (error) {
-        console.log(error)
-    }
-})
+const url = process.env.MONGOURI;
+const client = new MongoClient(url);
 
-const queryLog = async () => {
-    await console.log(queryResult)
+const dbName = "eclipses";
+                     
+function run() {
+    client.connect().then(() => {
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+        return db.collection('annular').find({date: '2035Mar09'}).toArray();
+    }).then((result) => {
+        console.log(result)
+    }).catch((err) => {
+            console.log(err.stack);
+    }).finally(() => {
+        client.close()
+    })
 }
 
-queryLog()
+run()
 
-connection.close()
+// const { ObjectID } = require('bson');
+// const mongoose = require('mongoose')
+// require('dotenv').config({
+//     path: `${__dirname}/.env`,
+//   });
+
+// const connection = mongoose.createConnection(process.env.MONGOURI);
+// const query = { type: 'annular' }
+// const Model = connection.model('annular', new mongoose.Schema({ date: String, type: String, coordinateData: Array, id: ObjectID }));
+
+// const gimme = async () => {
+// const queryResult = await Model.find({}).toArray();
+// console.log(queryResult)
+// }
+
+// gimme()
+
 
