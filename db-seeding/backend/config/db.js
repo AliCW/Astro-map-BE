f24ac67e4cc7dc896e30903ad1533a43bdf79a8a
plaintree <cheukhei04@hotmail.com>
2023-01-30 12:13:28 +0000

    add env variable inside config db.js

--- db-seeding/backend/config/db.js
+++ db-seeding/backend/config/db.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose")
-require('dotenv').config({
-    path: `${__dirname}/../../.env`,
-  });
+const mongoose = require("mongoose");
+const ENV = process.env.NODE_ENV || "development";
+require("dotenv").config({
+  path: `${__dirname}/../../.env.${ENV}`,
+});
 
 const connectDB = async () => {
-      try {
-            const conn = await mongoose.connect(process.env.MONGOURI)
-            console.log(`mongo db connected: ${conn.connection.host}`.cyan.underline)
-      } catch (error) {
-            console.log(error)
-            process.exit(1)
+  try {
+    const conn = await mongoose.connect(process.env.MONGOURI);
+    console.log(`mongo db connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-      }
-}
-
-module.exports = connectDB; 
+module.exports = connectDB;
