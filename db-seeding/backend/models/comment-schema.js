535f7f58b3fce03ff61c77f577db2867326f48df
AliCW <aliwhitrow1@gmail.com>
2023-02-01 16:43:46 +0000

    minor fixes created for GET comments

--- db-seeding/backend/models/comment-schema.js
+++ db-seeding/backend/models/comment-schema.js
@@ -1,7 +1,28 @@
 const mongoose = require("mongoose");
 
 // Create a new schema for our eclipse data
-x
+const commentSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  event: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: false,
+  },
+});
 
 // Create a mongoose model based on the above schema
 const Comment = mongoose.model("Comment", commentSchema);
